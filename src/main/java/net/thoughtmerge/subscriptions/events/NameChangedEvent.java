90dfff40fa78713a5245eba602c1082372ea1e3c
evangates <evan@thoughtmerge.net>
2013-06-23 23:15:08 -0700

    Added equals and hashCode to NameChangedEvent.

--- src/main/java/net/thoughtmerge/subscriptions/events/NameChangedEvent.java
+++ src/main/java/net/thoughtmerge/subscriptions/events/NameChangedEvent.java
@@ -8,15 +8,43 @@
 package net.thoughtmerge.subscriptions.events;
 
 import net.thoughtmerge.eventsourcing.Event;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 /**
  *
  * @author evan.gates
  */
 public class NameChangedEvent implements Event {
+
   public final String name;
 
   public NameChangedEvent(String name) {
     this.name = name;
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(3, 89)
+        .append(name)
+        .toHashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (obj == this) {
+      return true;
+    }
+    if (obj.getClass() != getClass()) {
+      return false;
+    }
+
+    final NameChangedEvent other = (NameChangedEvent) obj;
+    return new EqualsBuilder()
+        .append(name, other.name)
+        .isEquals();
   }
 }
